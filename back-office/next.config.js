f04900e2391e99805732454828d2aa0be84145cb
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2022-03-11 02:58:09 +0700

    Nextjs config

--- back-office/next.config.js
+++ back-office/next.config.js
@@ -1,6 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withAntdLess = require('next-plugin-antd-less')
 
-module.exports = nextConfig
+module.exports = withAntdLess({
+  lessVarsFilePath: './src/styles/variables.less',
+  lessVarsFilePathAppendToEndOfContent: false,
+  trailingSlash: true,
+  cssLoaderOptions: {
+    mode: 'local',
+    localIdentName: '[path][name]__[local]--[hash:base64:5]',
+    exportLocalsConvention: 'camelCase',
+    exportOnlyLocals: false,
+    getLocalIdent: (context, localIdentName, localName, options) => {
+      return 'whatever_random_class_name'
+    },
+  },
+
+  webpack(config) {
+    return config
+  },
+})
